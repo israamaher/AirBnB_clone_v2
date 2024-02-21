816029d175ccece6361eff35e363630757fb5420
amira-ahmed2 <amiraa.amin300@yahoo.com>
2024-02-21 20:49:48 +0200

    update

--- models/user.py
+++ models/user.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models import storage_mode
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models import storage_mode
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
+    __tablename__ = 'users'
     if storage_mode == 'db':
-        email = Column("email", String(128), nullable=False)
-        password = Column("password", String(128), nullable=False)
-        first_name = Column("first_name", String(128))
-        last_name = Column("last_name", String(128))
-        places = relationship(
-            "Place", backref="user", cascade="all, delete, delete-orphan")
-        reviews = relationship(
-            "Review", backref="user", cascade="all, delete, delete-orphan")
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete, delete-orphan')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete, delete-orphan')
     else:
         email = ""
         password = ""
