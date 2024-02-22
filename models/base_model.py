54982429e7ec25319ece1bc3b09e6e406edf824c
israa maher <you@example.com>
2024-02-21 16:51:40 -0800

    update base mode

--- models/base_model.py
+++ models/base_model.py
@@ -39,8 +39,8 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = self.__dict__.copy()
-        return '[{}] ({}) {}'.format(type(self).__name__, self.id, cls)
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
--- models/user.py
+++ models/user.py
@@ -9,14 +9,12 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    if models.storage_mode == 'db':
+    if models.storage_mode == 'db':       
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        # would not run with it in. This line was implemented in Task 8
         places = relationship('Place', backref='user', cascade='delete')
-        # Below line is commented out for caution and was added in Task 9
         reviews = relationship('Review', backref='user', cascade='delete')
     else:
         email = ""
